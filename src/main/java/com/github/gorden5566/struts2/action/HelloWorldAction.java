fda2ab3d075559c9f20cae04c4c15b7e60710f42
gorden5566 <gorden5566@126.com>
2016-11-25 22:04:56 +0800

    增加log4j日志

--- src/main/java/com/github/gorden5566/struts2/action/HelloWorldAction.java
+++ src/main/java/com/github/gorden5566/struts2/action/HelloWorldAction.java
@@ -1,8 +1,15 @@
 package com.github.gorden5566.struts2.action;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloWorldAction {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldAction.class);
+
     public String helloworld() {
+        logger.info("helloworld action!");
+
         return "success";
     }
 
