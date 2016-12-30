ba831d5eaf4fa38516eda6251603cff8120dfbae
gorden5566 <gorden5566@126.com>
2016-12-30 20:09:52 +0800

    捕获并处理sql异常

--- src/main/java/com/github/gorden5566/struts2/demos/dao/impl/UserDaoImpl.java
+++ src/main/java/com/github/gorden5566/struts2/demos/dao/impl/UserDaoImpl.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoImpl extends SqlMapClientDaoSupport implements UserDao {
@@ -14,21 +15,43 @@ public class UserDaoImpl extends SqlMapClientDaoSupport implements UserDao {
     @Override
     public User getUserById(Long id) {
         logger.info("userId: {}", id);
-        return (User) getSqlMapClientTemplate().queryForObject("getUserById", id);
+        try {
+            return (User) getSqlMapClientTemplate().queryForObject("getUserById", id);
+        } catch (Exception e) {
+            logger.error("getUserById", e);
+            return null;
+        }
     }
 
     @Override
     public List<User> getAllUser() {
-        return getSqlMapClientTemplate().queryForList("getAllUser");
+        try {
+            return getSqlMapClientTemplate().queryForList("getAllUser");
+        } catch (Exception e) {
+            logger.error("getAllUser error,", e);
+            return null;
+        }
+
     }
 
     @Override
     public Long addUser(User user) {
-        return (Long) getSqlMapClientTemplate().insert("addUser", user);
+        try {
+            return (Long) getSqlMapClientTemplate().insert("addUser", user);
+        } catch (Exception e) {
+            logger.error("addUser error,", e);
+            return null;
+        }
     }
 
     @Override
     public Integer deleteUserById(Long id) {
-        return getSqlMapClientTemplate().delete("deleteUserById", id);
+        try {
+            return getSqlMapClientTemplate().delete("deleteUserById", id);
+        } catch (Exception e) {
+            logger.error("deleteUserById error,", e);
+            return null;
+        }
+
     }
 }
